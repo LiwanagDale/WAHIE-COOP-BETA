5c80bd1642b391ddd6d220f3baea3d31fa83c608
Gvan-Manalo <123857731+Gvan-Manalo@users.noreply.github.com>
2023-03-15 13:44:12 +0800

    Update NotificationController.php

--- BACKEND/app/Http/Controllers/NotificationController.php
+++ BACKEND/app/Http/Controllers/NotificationController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    public function sendSmsNotification()
+    public function sendSmsNotification(Request $request)
     {
-        $basic  = new \Vonage\Client\Credentials\Basic("f3a1f673", "14dYfIOffydUOMoo");
+        $mobile = $request->mobile;
+        $basic  = new \Vonage\Client\Credentials\Basic("a2099d81", "RLhOCBdklPsMz9DF");
         $client = new \Vonage\Client($basic);
 
         $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS("639072479844", 'BRAND_NAME', 'A text message sent using the Nexmo SMS API')
-        );
+            new \Vonage\SMS\Message\SMS($mobile, 'BRAND_NAME', 'A text message sent using the Nexmo SMS API')
+        ); 
         
         $message = $response->current();
         
@@ -23,4 +25,11 @@ class NotificationController extends Controller
             echo "The message failed with status: " . $message->getStatus() . "\n";
         }
     }
+
+
+public function member_create()
+{
+    $data = Member::all();
+    return view('member_create',['members' => $data]);
+} 
 }
