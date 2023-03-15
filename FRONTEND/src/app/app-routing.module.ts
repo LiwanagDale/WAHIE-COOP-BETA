1d1c4d64d360f85cfd5fc95b45ee871b5b90a141
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2023-03-15 14:16:52 +0800

    FrontEnd and Animation of Login
    FrontEnd and Animation of Login and Signup

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -1,10 +1,54 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
+import { CheckEmailComponent } from './check-email/check-email.component';
+import { NewPassComponent } from './new-pass/new-pass.component';
+import { PassDoneComponent } from './pass-done/pass-done.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'login', 
+    component: LoginComponent
+  },
+  {
+    path: 'register', 
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'forgot-pass', 
+    component: ForgotPassComponent
+  },
+  {
+    path: 'check-email', 
+    component: CheckEmailComponent
+  },
+  {
+    path: 'new-pass', 
+    component: NewPassComponent 
+  },
+  {
+    path: 'pass-done', 
+    component: PassDoneComponent 
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
