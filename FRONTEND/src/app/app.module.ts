a5015dc472fb05ee4c86ddf0d68e9a4269d1b863
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2023-03-15 14:19:27 +0800

    FrontEnd and Animation of Login
    FrontEnd and Animation of Login and Signup

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -1,32 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+
 import { AppRoutingModule } from './app-routing.module';
-import { RegisterComponent } from './register/register.component';
-import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
-import { CheckEmailComponent } from './check-email/check-email.component';
-import { NewPassComponent } from './new-pass/new-pass.component';
-import { PassDoneComponent } from './pass-done/pass-done.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    ForgotPassComponent,
-    CheckEmailComponent,
-    NewPassComponent,
-    PassDoneComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
