481d700345b4fc50f5e4d2e3507353c4b27cbd07
EclaviaAljohn <aljohneclavia@gmail.com>
2023-03-17 14:10:16 +0800

    Update journalentry blade

--- BACKEND/resources/views/accounting/journalentry.blade.php
+++ BACKEND/resources/views/accounting/journalentry.blade.php
@@ -6,6 +6,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
     <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 <body>
   <div class="container">
@@ -20,46 +21,49 @@
       <input type="number" name="journalEntry_number" class="form-control" required>
     </div>
     </div>
-    <table class="table table-bordered">
-        <thead>
-           <tr>
-              <th></th>
-              <th>No.</th>
-              <th>Account</th>
-              <th>Debit</th>
-              <th>Credit</th>
-              <th>Description</th>
-              <th>Name</th>
-           </tr> 
-        </thead>
-        <tbody>
-              <tr>
-                <td></td>
-                 <td></td>
-                 <td>
-                  <select class="form-select" aria-label="Default select example">
-                    <option selected>Select Journal Entry</option>
-                    @foreach($journals as $journal)
-                        <option>{{$journal->journal_number}} {{$journal->journal_name}} {{$journal->journal_type}}</option>
-                    @endforeach
-                  </select>
-                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Add+</button>
-                 </td>
-                 <td>
-                    <input type="text" inputmode="numeric" name="debit" class="form-control" required>
-                 </td>
-                 <td>
-                    <input type="text" inputmode="numeric" name="credit" class="form-control" required>
-                 </td>
-                 <td>
-                  <input type="text"  name="description" class="form-control" required>
-                </td>
-                <td>
-                  <input type="text" name="name" class="form-control" required>
-                </td>
-              </tr>
-        </tbody>
-    </table>
+    <table class="table table-bordered" id="dynamicAddRemove">
+      <tr>
+          <th></th>
+          <th>No.</th>
+          <th>Account</th>
+          <th>Debit</th>
+          <th>Credit</th>
+          <th>Description</th>
+          <th>Name</th>
+          <th></th>
+      </tr>
+      <tr>
+          <td>
+              <button type="button" name="add" id="dynamic-ar" class="btn btn-outline-primary"><i class="bi bi-plus-circle"></i></button>
+          </td>
+          <td>
+          </td>
+          <td>
+              <input type="text" inputmode="numeric" class="form-control input-sm"  name="test[0][debit]">
+              <select class="form-select" aria-label="Default select example" name="test[0][journal]" >
+                  <option selected>Select Journal Entry</option>
+                  @foreach($journals as $journal)
+                  <option>{{$journal->journal_number}} {{$journal->journal_name}} {{$journal->journal_type}}</option>
+                  @endforeach
+              </select>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Add+</button>
+          </td>
+          <td>
+              <input type="text" inputmode="numeric" class="form-control input-sm"  name="test[0][debit]">
+          </td>
+          <td>
+              <input type="text" inputmode="numeric" class="form-control input-sm"  name="test[0][credit]">
+          </td>
+          <td>
+              <input type="text" class="form-control"  name="test[0][description]">
+          </td>
+          <td>
+              <input type="text" class="form-control"  name="test[0][name]">
+          </td>
+          <td>
+          </td>
+      </tr>
+  </table>
     <tfoot>
       <button type="button" class="btn btn-primary" >Cancel</button>
       <button type="button" class="btn btn-primary" >Save</button>
@@ -120,5 +124,48 @@ const myInput = document.getElementById('exampleModal')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+</script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript">
+  var i = 0;
+  $("#dynamic-ar").click(function () {
+      ++i;
+      $("#dynamicAddRemove").append(
+          `<tr>
+              <td>
+              </td>
+              <td>
+              </td>
+              <td>
+                  <select class="form-select" aria-label="Default select example" name="test[`+i+`][journal]">
+                      <option selected>Select Journal Entry</option>
+                      @foreach($journals as $journal)
+                          <option>{{$journal->journal_number}} {{$journal->journal_name}} {{$journal->journal_type}}</option>
+                      @endforeach
+                  </select>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Add+</button>
+              </td>
+              <td>
+                  <input type="text" inputmode="numeric" class="form-control input-sm"  name="test[`+i+`][debit]">
+              </td>
+              <td>
+                  <input type="text" inputmode="numeric" class="form-control input-sm"  name="test[`+i+`][credit]">
+              </td>
+              <td>
+                  <input type="text" class="form-control input-sm"  name="test[`+i+`][description]">
+              </td>
+              <td>
+                  <input type="text" class="form-control"  name="test[`+i+`][name]">
+              </td>
+              <td>
+                  <button type="button" class="btn btn-outline-danger remove-input-field"><i class="bi bi-x-circle"></i></button>
+              </td>
+          </tr>`
+          );
+  });
+  $(document).on('click', '.remove-input-field', function () {
+      $(this).parents('tr').remove();
+  });
 </script>
 @endsection
